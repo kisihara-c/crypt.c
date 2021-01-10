8e0911db8c18c245cabdc4b3ee7bf8a9e2e10fb8
kisihara_c <kisihara.c@gmail.com>
2021-01-10 17:30:25 +0900

    mockMaking

--- public/cryptCore.js
+++ public/cryptCore.js
@@ -1,2 +1,4 @@
 var hash = forge.md.md5.create();hash.update("test");
-document.write(hash.digest().toHex());
+//document.write(hash.digest().toHex());
+
+const alertAdd = ()=>{document.write(hash2.digest().toHex());};
