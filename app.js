29509d2e8e49e2733ccae4adb67df39622544de7
kisihara_c <kisihara.c@gmail.com>
2021-01-15 01:48:32 +0900

    Revert "helmetInstall"
    This reverts commit d0134ebb263feb41eda3acc17fea10954efca317.

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT||3000;
 
-var helmet = require('helmet');
-app.use(helmet());
 app.use(express.static('public'));
 
 app.listen(port,() =>{
