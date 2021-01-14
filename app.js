02592767d99b39fe1d35559d8da31b4ca22f6af4
kisihara_c <kisihara.c@gmail.com>
2021-01-15 01:56:42 +0900

    disableXpowered

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT||3000;
 
+app.disable('x-powered-by');
+
 app.use(express.static('public'));
 
 app.listen(port,() =>{
